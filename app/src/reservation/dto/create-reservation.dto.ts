6f4e3567a0ce68172d2551d0f8495276bd65be02
mizuki sato <mizki.sato.0025@gmail.com>
2025-01-18 14:12:41 +0900

    create createReservationDto

--- app/src/reservation/dto/create-reservation.dto.ts
+++ app/src/reservation/dto/create-reservation.dto.ts
@@ -1,1 +1,16 @@
-export class CreateReservationDto {}
+import { IsNumber, IsNotEmpty, IsISO8601 } from 'class-validator';
+
+export class CreateReservationDto {
+    @IsNumber()
+    @IsNotEmpty()
+    car_id: number;
+  
+    @IsISO8601()
+    @IsNotEmpty()
+    startTime: string;
+  
+    @IsISO8601()
+    @IsNotEmpty()
+    endTime: string;
+}
+  
--- app/src/reservation/reservation.service.ts
+++ app/src/reservation/reservation.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateReservationDto } from './dto/create-reservation.dto';
-import { UpdateReservationDto } from './dto/update-reservation.dto';
 
 @Injectable()
 export class ReservationService {
@@ -8,19 +7,7 @@ export class ReservationService {
     return 'This action adds a new reservation';
   }
 
-  findAll() {
-    return `This action returns all reservation`;
-  }
-
   findOne(id: number) {
     return `This action returns a #${id} reservation`;
-  }
-
-  update(id: number, updateReservationDto: UpdateReservationDto) {
-    return `This action updates a #${id} reservation`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} reservation`;
   }
 }
