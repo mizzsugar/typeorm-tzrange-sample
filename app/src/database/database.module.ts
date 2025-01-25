8141b16fbbc9c7ac26baac48002b366d22b6424d
mizuki sato <mizki.sato.0025@gmail.com>
2025-01-25 20:06:22 +0900

    add reservation repository to database module

--- app/src/database/database.module.ts
+++ app/src/database/database.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarEntity } from './entity/car.entity';
 import { ReservationEntity } from './entity/reservation.entity';
 import { CarRepository } from './repository/car.repository';
+import { ReservationRepository } from './repository/reservation.repository';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([CarEntity, ReservationEntity])
   ],
-  providers: [CarRepository],
-  exports: [CarRepository],
+  providers: [CarRepository, ReservationRepository],
+  exports: [CarRepository, ReservationRepository],
 })
 export class DatabaseModule {}
--- app/src/reservation/reservation.module.ts
+++ app/src/reservation/reservation.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReservationService } from '~/reservation/reservation.service';
 import { ReservationController } from '~/reservation/reservation.controller';
+import { DatabaseModule } from '~/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ReservationController],
   providers: [ReservationService],
 })
